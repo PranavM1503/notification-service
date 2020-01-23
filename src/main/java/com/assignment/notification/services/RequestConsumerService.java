ac3d6197d3a6c4a634eacc6c43340af77c07e4eb
pranavmahajan <pranavm1503@gmail.com>
2020-01-23 11:42:27 +0530

    Elastic Search configuration added

--- src/main/java/com/assignment/notification/services/RequestConsumerService.java
+++ src/main/java/com/assignment/notification/services/RequestConsumerService.java
@@ -1,5 +1,6 @@
 package com.assignment.notification.services;
 
+import com.assignment.notification.dto.SmsDetailsForElasticSearch;
 import com.assignment.notification.entities.SmsApiModel;
 import com.assignment.notification.entities.SmsRequest;
 import com.assignment.notification.repositories.smsRequestRepository;
@@ -30,9 +31,11 @@ import java.util.logging.*;
 public class RequestConsumerService {
     private final Logger logger = LoggerFactory.getLogger(Producer.class);
     private smsRequestRepository smsRequestRepository;
+    private ElasticSearchService elasticSearchService;
     @Autowired
-    public RequestConsumerService(smsRequestRepository smsRequestRepository){
+    public RequestConsumerService(smsRequestRepository smsRequestRepository, ElasticSearchService elasticSearchService){
         this.smsRequestRepository = smsRequestRepository;
+        this.elasticSearchService = elasticSearchService;
     }
 
     Jedis jedis = new Jedis();
@@ -50,51 +53,59 @@ public class RequestConsumerService {
         logger.info(String.format("Phone Number is : %s", phoneNumber));
         boolean isBlackListed = jedis.sismember("phone_number", phoneNumber);
 
-        if(isBlackListed == true){
+        //dto for elastic search service
+        SmsDetailsForElasticSearch smsDetailsForElasticSearch = new SmsDetailsForElasticSearch(smsRequest.getId(), smsRequest.getRequestId(),
+                smsRequest.getPhoneNumber(), smsRequest.getMessage(), smsRequest.getCreatedAt(), smsRequest.getUpdatedAt());
+
+
+
+        if(isBlackListed){
             logger.info("Blacklisted number");
         }
-//        else{
-//            logger.info("Not Blacklisted");
-//        }
-//
         else{
-            String apiUrl = "https://api.imiconnect.in/resources/v1/messaging";
-
-            RestTemplate restTemplate = new RestTemplate();
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Content-Type", "application/json");
-            headers.set("key", "7b73f76d-369e-11ea-9e4e-025282c394f2");
-
-            List <String> msisdn = new ArrayList<>();
-            msisdn.add(smsRequest.getPhoneNumber());
-
-
-            SmsApiModel smsApiModel = new SmsApiModel(smsRequest.getMessage(), msisdn, smsRequest.getRequestId());
-
-//   ********************         json conversion to verify the post request           *******************
-            ObjectMapper objectMapper = new ObjectMapper();
-
-            try {
-                String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(smsApiModel);
-                logger.info("\n"  + json);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-//   ******************************************************************************************************
-            HttpEntity<SmsApiModel> httpRequest = new HttpEntity<>(smsApiModel, headers);
-            logger.info(String.format(httpRequest.toString()));
-            ResponseEntity<String> response = restTemplate.postForEntity(apiUrl, httpRequest, String.class);
-
-            logger.info(String.format(response.toString()));
-            if (response.getStatusCode() == HttpStatus.CREATED) {
-                logger.info("Request Successful");
-                logger.info(response.getBody());
-            }else{
-//                logger.info("Request Failed");
-                logger.info(String.format(response.getStatusCode().toString()));
-            }
-
+            logger.info("Not Blacklisted \n");
+            elasticSearchService.indexSmsData(smsDetailsForElasticSearch);
+            elasticSearchService.getSmsDatabyId(Integer.toString(smsDetailsForElasticSearch.getId()));
         }
+//
+//        else{
+//            String apiUrl = "https://api.imiconnect.in/resources/v1/messaging";
+//
+//            RestTemplate restTemplate = new RestTemplate();
+//
+//            HttpHeaders headers = new HttpHeaders();
+//            headers.set("Content-Type", "application/json");
+//            headers.set("key", "7b73f76d-369e-11ea-9e4e-025282c394f2");
+//
+//            List <String> msisdn = new ArrayList<>();
+//            msisdn.add(smsRequest.getPhoneNumber());
+//
+//
+//            SmsApiModel smsApiModel = new SmsApiModel(smsRequest.getMessage(), msisdn, smsRequest.getRequestId());
+//
+////   ********************         json conversion to verify the post request           *******************
+//            ObjectMapper objectMapper = new ObjectMapper();
+//
+//            try {
+//                String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(smsApiModel);
+//                logger.info("\n"  + json);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+////   ******************************************************************************************************
+//            HttpEntity<SmsApiModel> httpRequest = new HttpEntity<>(smsApiModel, headers);
+//            logger.info(String.format(httpRequest.toString()));
+//            ResponseEntity<String> response = restTemplate.postForEntity(apiUrl, httpRequest, String.class);
+//
+//            logger.info(String.format(response.toString()));
+//            if (response.getStatusCode() == HttpStatus.CREATED) {
+//                logger.info("Request Successful");
+//                logger.info(response.getBody());
+//            }else{
+////                logger.info("Request Failed");
+//                logger.info(String.format(response.getStatusCode().toString()));
+//            }
+//
+//        }
     }
 }
